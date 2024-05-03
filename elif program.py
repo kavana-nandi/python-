061abbc8bcf48dea41371b5e284136fee6a77f67
kavana-nandi <167275309+kavana-nandi@users.noreply.github.com>
2024-05-03 14:36:29 +0530

    elif program.py

---
+++ elif program.py
@@ -0,0 +1,10 @@
+
+height=float(input())
+if(height>6):
+    print("tall")
+elif(height>=4 and height<=6):
+    print("average")
+elif(height<4):
+    print("short")
+else:
+    print("height does not match")        
